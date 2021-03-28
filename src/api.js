beacff6aee64ac7f48bdb0bd445b95921590cf16
binoy638 <binoy638@gmail.com>
2021-03-28 23:38:31 +0530

    changed api endpoints

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 // const BASE_URL =  "http://localhost:5000"
 const BASE_URL = "https://playit-server.herokuapp.com";
 
-export const newReleaseURL = `${BASE_URL}/new-release`;
-export const hotTracksURL = `${BASE_URL}/top-tracks`;
+export const newReleaseURL = `${BASE_URL}/search/new-release`;
+export const hotTracksURL = `${BASE_URL}/search/top-tracks`;
 export const fetchVideoURL = (query) =>
-  `${BASE_URL}/videoid?query=${query} (lyrics)`;
-export const searchTracksURL = (query) => `${BASE_URL}/search?query=${query}`;
+  `${BASE_URL}/search/videoid?query=${query} (lyrics)`;
+export const searchTracksURL = (query) =>
+  `${BASE_URL}/search/track?query=${query}`;
