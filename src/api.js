63a7d6f58f2e1b030add4d6be81959d95d86dfa9
binoy638 <binoy638@gmail.com>
2021-03-30 17:48:57 +0530

    changed url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // const BASE_URL =  "http://localhost:5000"
-const BASE_URL = "http://144.202.14.194";
+const BASE_URL = "https://playit-server.herokuapp.com";
 
 export const newReleaseURL = `${BASE_URL}/search/new-release`;
 export const hotTracksURL = `${BASE_URL}/search/top-tracks`;
