8f135f156dcbbdaa2613aa5779bfbca81bc8eb93
binoy638 <binoy638@gmail.com>
2021-05-30 19:51:21 +0530

    changed base url

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://playit.sytes.net";
-// const BASE_URL = "https://playit-server.herokuapp.com";
-const baseURL = "http://localhost:5000";
+// const baseURL = "https://playit.sytes.net";
+const baseURL = "https://playit-server.herokuapp.com";
+// const baseURL = "http://localhost:5000";
 
 export const API = axios.create({ baseURL });
