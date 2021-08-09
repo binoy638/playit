b2d533f2a2c9edbcfd5d768bc38a5992d31c1efa
binoy638 <binoy638@gmail.com>
2021-08-09 14:42:29 +0530

    added baseURL to env

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// export const baseURL = "https://fire-platinum-licorice.glitch.me/";
-export const baseURL = "https://playit-server.herokuapp.com";
-// export const baseURL = "http://localhost:5001";
+export const baseURL = process.env.BASE_URL || "http://localhost:5001";
 
 export const API = axios.create({ baseURL });
